0050eeaa0255f2d4991e56376c4827916491387f
satishavhad <satishavhad@gmail.com>
2017-12-03 21:46:25 -0500

    Add files via upload

---
+++ 04 ReverseTransaction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 18:56:41 2017
+
+@author: avhadsa
+"""
+
+def ReverseTransaction(Transaction_Type, Activity_Id, Transaction_Id, Transaction_Suppression="Statements and Contract Notes/Advices", Reason_code):
+    
+    return  Reversal_Activity_ID, Transaction_Type, Activity_Id, Transaction_Id, Account_Number, Account_Name, Portfolio, Asset, Transactio_date, Trade_Date, Settle_Date, Net_Amount
+
